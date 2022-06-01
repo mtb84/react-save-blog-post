388190b683d28e7744730f4af57c5eb99f384d64
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-01 13:03:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+    <form action="">
+      <label htmlFor="">
+        <input type="text" name="name" />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </label>
+    </form>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
