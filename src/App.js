e40f5dd15279014a2c7ace512c42b03e2b5b9908
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-02 13:36:27 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import {useState, useEffect} from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [title, setTitle] = useState("")
+  const [body, setBody] = useState("");
+  const [posts, setPosts] = useState([])
+
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const updateBody = (e) => {
+    setBody(e.target.value)
+  }
+
+  const addPost = (e) => {
+    const newPost = {
+      title,
+      body,
+    };
+    setPosts([...posts, newPost]);
+    setTitle("");
+    setBody("");
+
+
+  }
+
+
   return (
-    <div className="App">
+    
+      <form onSubmit={addPost}>
+          <h2>Write and Save Blog</h2>
+          <label htmlFor="title">Blog Title</label> <br />
+          <input id="title" type="text" name='title' value={title} onChange={updateTitle} />
+          <br />
+          <label htmlFor='blog' >New Blog</label> <br />
+          <textarea value={body} onChange={updateBody}> </textarea> <br />
+          <button type="submit">Save</button>
+      </form>
 
-
-    <form action="">
-      <label htmlFor="">
-        <input type="text" name="name" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-      </label>
-    </form>
-
-
-
-
-    </div>
-  );
-}
+  
+)};
 
 export default App;
